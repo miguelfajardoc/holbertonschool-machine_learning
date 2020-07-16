f27c1d774e1f0903463ea8be86f829a907749bde
miguelfajardoc <775@holbertonschool.com>
2020-07-16 18:07:35 -0500

    [fix] style pep8

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
 
+
 def matrix_shape(mat1):
-  x = np.array(mat1)
-  return [ element for element in x.shape]
+    x = np.array(mat1)
+    return [element for element in x.shape]
