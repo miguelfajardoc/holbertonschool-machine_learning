8d1ab883fc858cf8152bb04b3693eaab1a8e8780
miguelfajardoc <775@holbertonschool.com>
2020-07-17 04:36:14 +0000

    [add] howdy partner

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" concatenates 2 list and return the concatenation in a new list """
+
+
+def cat_arrays(arr1, arr2):
+    """ concatenates 2 list and return the concatenation in a new list """
+
+    cat = []
+    cat.append(arr1)
+    cat.append(arr2)
+    return cat
+    
