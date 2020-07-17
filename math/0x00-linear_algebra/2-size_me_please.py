b7cba82decf3430f19cd7ceed948e74cc557ed19
miguelfajardoc <775@holbertonschool.com>
2020-07-17 02:48:22 +0000

    fix 2 point

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -4,9 +4,10 @@
 def matrix_shape(mat1):
     shape = []
     while(1):
-        try:
+        if mat1 == aValidList:
             shape.append(len(mat1))
-        except:
+        else:
             return shape
         mat1 = mat1[0]
+
 
