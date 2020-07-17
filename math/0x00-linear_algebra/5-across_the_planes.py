5747315f47c922345beb47422f0a79b14d32b571
miguelfajardoc <775@holbertonschool.com>
2020-07-17 04:20:36 +0000

    add across planes

---
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" add wisely matrices """
+
+
+def add_matrices2D(mat1, mat2):
+    """ function that add wisely matrices """
+
+    if matrix_shape(mat1) != matrix_shape(mat2):
+        return None
+
+    matrixAddition = []
+    for row in range(len(mat1)):
+        rowAddition = []
+        for columns in range(len(mat1)):
+            rowAddition.append(mat1[row][columns] + mat2[row][columns])
+
+        matrixAddition.append(rowAddition)
+
+    return matrixAddition
+
+
+def matrix_shape(mat1):
+    """Return the shape of the matrix in all of his dimensions"""
+
+    shape = []
+    while(1):
+        if isinstance(mat1, list):
+            shape.append(len(mat1))
+        else:
+            return shape
+        mat1 = mat1[0]
