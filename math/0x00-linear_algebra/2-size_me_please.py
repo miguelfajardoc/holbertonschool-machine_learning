e11daf572be65840a56641d903bb988dc62e0f5b
miguelfajardoc <775@holbertonschool.com>
2020-07-17 02:40:49 +0000

    [add] 2 size me please

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
-import numpy as np
 
 
 def matrix_shape(mat1):
-    x = np.array(mat1)
-    return [element for element in x.shape]
+    shape = []
+    while(1):
+        try:
+            shape.append(len(mat1))
+        except:
+            return shape
+        mat1 = mat1[0]
+
