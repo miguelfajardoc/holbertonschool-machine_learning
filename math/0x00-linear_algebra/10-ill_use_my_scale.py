96efcc9799daff3ab3a72ddc1b3ccfbb2a782209
miguelfajardoc <775@holbertonschool.com>
2020-07-19 03:24:15 +0000

    [add] numpy shape

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" calculate the shape of a matrix numpy.ndarray """
+
+
+def np_shape(matrix):
+    """ calculate the shape of a matrix numpy.ndarray """
+
+    return matrix.shape
