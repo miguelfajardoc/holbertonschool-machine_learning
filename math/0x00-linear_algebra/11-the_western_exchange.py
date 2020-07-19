c0519445429e144016be3f361c096db06e15a2e6
miguelfajardoc <775@holbertonschool.com>
2020-07-19 03:47:14 +0000

    [add] transpose numpy

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" transpose a matrix with that is ndarray """
+
+
+def np_transpose(matrix):
+    """ transpose a matrix with that is ndarray """
+
+    return matrix.transpose()
