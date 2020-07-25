e5f483bcdab1d3e83bb8e551b961d558ee46c12c
miguelfajardoc <775@holbertonschool.com>
2020-07-25 20:48:45 +0000

    [fix] condition when is negative

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -7,7 +7,7 @@ def summation_i_squared(n):
 
     if (not isinstance(n, int)) and (not isinstance(n, float)):
         return None
-    if n < 0:
+    if n <= 0:
         return None
     n = int(n)
     summatory = (n * (n + 1) * (2 * n + 1)) / 6
