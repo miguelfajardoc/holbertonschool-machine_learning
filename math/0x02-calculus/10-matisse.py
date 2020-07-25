bbf22e4888aac0b5b1a787225795cd73aba70cd9
miguelfajardoc <775@holbertonschool.com>
2020-07-25 20:37:56 +0000

    [add] matisse

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Derivate polinomial with list """
+
+def poly_derivative(poly):
+    """ Derivate polinomial with list """
+    
+    if not isinstance(poly, list):
+        return None
+    if len(poly) == 0:
+        return [0]
+
+    for i in range(2, len(poly)):
+        poly[i] *= i
+    poly.pop(0)
+    
+    return poly
--- math/0x02-calculus/9-main.py
+++ math/0x02-calculus/9-main.py
@@ -4,5 +4,5 @@ summation_i_squared = __import__('9-sum_total').summation_i_squared
 
 n = "56ds5"
 print(summation_i_squared(n))
-n = 5.5
+n = 7
 print(summation_i_squared(n))
