53743039db910044c7166f71ecde933713bbee73
miguelfajardoc <775@holbertonschool.com>
2020-07-26 19:45:56 +0000

    [add] double and python integrate

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" integrate this! """
+
+
+def poly_integral(poly, C=0):
+    """ function that calculate the integral of a polinomial function """
+
+    if not isinstance(poly, list) or not isinstance(C, (int, float)):
+        return None
+
+    for i in range(len(poly)):
+        poly[i] /= (i + 1)
+        if poly[i] == 0 or poly[i] - int(poly[i]) == 0:
+            poly[i] = int(poly[i])
+    poly.insert(0, C)
+    return poly
---
+++ math/0x02-calculus/17-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+poly_integral = __import__('17-integrate').poly_integral
+
+poly = [5, 3, 0, 1]
+print(poly_integral(poly, 6))
+poly = []
+print(poly_integral(poly))
+poly = "hola"
+print(poly_integral(poly))
+poly = [5, -8, 0, 1]
+print(poly_integral(poly, "ddd"))
