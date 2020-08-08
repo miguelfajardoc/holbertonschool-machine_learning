e256547b62ca7f5a729f9be4431a543e749dd02b
miguelfajardoc <775@holbertonschool.com>
2020-08-08 00:09:46 +0000

    [add] first supervised_learning commit

---
+++ supervised_learning/0x00-binary_classification/0-neuron.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Neuron fileeee"""
+
+import numpy as np
+
+
+class Neuron:
+    """ Neuron that perfom a binary classification
+        Args:
+             - nx: number of input features to the neuron
+             - W: weights vector for the neuron.
+             - b: The bias for the neuron
+             - A: The activated outpot of the neuron (prediction)
+    """
+
+    def __init__(self, nx):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer");
+        if nx < 1:
+            raise ValueError("nx must be a positive integer");
+
+        self.W = np.random.normal(size=(1, nx))
+        self.b = 0
+        self.A = 0
