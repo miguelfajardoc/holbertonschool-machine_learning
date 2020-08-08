927ec1e40c0c780954269ccf6dc4937c5343d271
miguelfajardoc <775@holbertonschool.com>
2020-08-08 00:15:08 +0000

    [fix] remove semicolons in python, jajajaja

--- supervised_learning/0x00-binary_classification/0-neuron.py
+++ supervised_learning/0x00-binary_classification/0-neuron.py
@@ -15,9 +15,9 @@ class Neuron:
 
     def __init__(self, nx):
         if not isinstance(nx, int):
-            raise TypeError("nx must be an integer");
+            raise TypeError("nx must be an integer")
         if nx < 1:
-            raise ValueError("nx must be a positive integer");
+            raise ValueError("nx must be a positive integer")
 
         self.W = np.random.normal(size=(1, nx))
         self.b = 0
