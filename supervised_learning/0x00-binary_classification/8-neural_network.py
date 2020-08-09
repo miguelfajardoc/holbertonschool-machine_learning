03c888a2b5ac3210e3b4afd0169901ea13a2ad68
miguelfajardoc <775@holbertonschool.com>
2020-08-09 05:20:39 +0000

    [add] create neural network class

---
+++ supervised_learning/0x00-binary_classification/8-neural_network.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+""" Module that defines a neural network
+"""
+
+import numpy as np
+
+class NeuralNetwork:
+    """ NeuralNetwork that defines a neural network with one hidden
+        layer performing binary classification
+    Public instance attributes
+        W1: The weights vector for the hidden layer.
+        b1: The bias for the hidden layer.
+        A1: The activated output for the hidden layer.
+        W2: The weights vector for the output neuron.
+        b2: The bias for the output neuron.
+        A2: The activated output for the output neuron (prediction).
+    """
+
+    def __init__(self, nx, nodes):
+        """ Constructor class
+        - nx is the number of input features
+        - nodes is the number of nodes found in the hidden layer
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.nodes = nodes
+        self.W1 = np.random.normal(size=(nodes, nx))
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.normal(size=(1, nodes))
+        self.b2 = 0
+        self.A2 = 0
