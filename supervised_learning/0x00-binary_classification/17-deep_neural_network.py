fbc604365d5fd22275ae7ab14154bf4d4aca3642
miguelfajardoc <775@holbertonschool.com>
2020-08-10 02:44:38 +0000

    [feature] privatice

---
+++ supervised_learning/0x00-binary_classification/17-deep_neural_network.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+""" Module that defines a deep neural network
+"""
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """ DeepNeuralNetwork that defines a deep neural network with one hidden
+        layer performing binary classification
+    Public instance attributes
+        W1: The weights vector for the hidden layer.
+        b1: The bias for the hidden layer.
+        A1: The activated output for the hidden layer.
+        W2: The weights vector for the output neuron.
+        b2: The bias for the output neuron.
+        A2: The activated output for the output neuron (prediction).
+    """
+
+    def __init__(self, nx, layers):
+        """ Constructor class
+        - nx is the number of input features
+        - layers is a list representing the number of nodes in each layer
+          of the network
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list):
+            raise TypeError("layers must be a list of positive integers")
+        if len(layers) == 0:
+            raise TypeError('layers must be a list of positive integers')
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        prev_inputs = nx
+        for l in range(len(layers)):
+            if not isinstance(layers[l], int) or layers[l] <= 0:
+                raise TypeError("layers must be a list of positive integers")
+            key = 'W{}'.format(l + 1)
+            bias = 'b{}'.format(l + 1)
+            self.__weights[key] = np.random.randn(
+                layers[l], prev_inputs) * np.sqrt(2 / prev_inputs)
+            self.__weights[bias] = np.zeros((layers[l], 1))
+            prev_inputs = layers[l]
+
+    @property
+    def L(self):
+        return self.__L
+
+    @property
+    def cache(self):
+        return self.__cache
+
+    @property
+    def weights(self):
+        return self.__weights
