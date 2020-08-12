6bbaa4462b7b63abf9a63702268e9d20117ea779
miguelfajardoc <775@holbertonschool.com>
2020-08-12 02:15:34 +0000

    [fix] add restriction

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -9,14 +9,19 @@ def one_hot_encode(Y, classes):
     """
     Hot encode a vector to a one-hot matrix
     """
-    if not isinstance(Y, list) or not isinstance(classes, int):
+    if not (isinstance(Y, np.ndarray) or isinstance(classes, int)):
+        print("aa")
+        return None
+    if Y.max() > classes:
+        print("here")
+        return None
+    if Y.min() < 0:
+        print("here?")
         return None
     m = len(Y)
     HM = np.zeros((classes, m))
     i = 0
     while i < m:
-        if Y[i] > classes:
-            return None
         HM[Y[i]][i] = 1
         i += 1
     return HM
