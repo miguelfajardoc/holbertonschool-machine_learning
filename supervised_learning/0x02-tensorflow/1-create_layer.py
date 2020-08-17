b11a7046620da16ed7d063d95ed59de932c5af15
miguelfajardoc <775@holbertonschool.com>
2020-08-17 04:58:13 +0000

    [add] layer

---
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Module that contains a function layer tensorFlow"""
+
+import tensorflow as tf
+
+
+def create_layer(prev, n, activation):
+    """
+    prev is the tensor output of the previous layer
+    n is the number of nodes in the layer to create
+    activation is the activation function that the layer should use
+    use tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG") to
+    implement He et. al initialization for the layer weights
+    each layer should be given the name layer
+    Returns: the tensor output of the layer
+
+    """
+    return tf.layers.dense(prev, units=n, activation=activation, name="layer",
+                           kernel_initializer=tf.contrib.layers.
+                           variance_scaling_initializer(mode="FAN_AVG"))
