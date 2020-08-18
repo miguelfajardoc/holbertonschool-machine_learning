769992c5fef5dba608b3ed4d0f26162158cf43e3
miguelfajardoc <775@holbertonschool.com>
2020-08-18 04:30:15 +0000

    [add] add_to_collection the elements to save

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -29,12 +29,19 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
     """
     # placeholders
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
+    tf.add_to_collection('x', x)
+    tf.add_to_collection('y', y)
     # tensors
     y_pred = forward_prop(x, layer_sizes, activations)
     loss = calculate_loss(y, y_pred)
     accuracy = calculate_accuracy(y, y_pred)
+    tf.add_to_collection('y_pred', y_pred)
+    tf.add_to_collection('loss', loss)
+    tf.add_to_collection('accuracy', accuracy)
     # Operations
     train_op = create_train_op(loss, alpha)
+    tf.add_to_collection('train_op', train_op)
+
     init = tf.global_variables_initializer()
     saver = tf.train.Saver()
 
