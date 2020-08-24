5730168a60026cf36d26571e154569d3e1b14169
miguelfajardoc <775@holbertonschool.com>
2020-08-24 05:13:26 +0000

    [add] decay tensor

---
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module of create RMSprop in tensorflow
+"""
+import tensorflow as tf
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """
+    function  that creates the training operation for a neural network in
+    tensorflow using the RMSProp optimization algorithm:
+    loss is the loss of the network
+    alpha is the learning rate
+    beta2 is the RMSProp weight
+    epsilon is a small number to avoid division by zero
+    Returns: the RMSProp optimization operation
+    """
+    decay_optimizer = tf.train.inverse_time_decay(alpha, global_step,
+                                                  decay_steps, decay_rate,
+                                                  epsilon=epsilon)
+    return decay_optimizer.minimize(loss)
