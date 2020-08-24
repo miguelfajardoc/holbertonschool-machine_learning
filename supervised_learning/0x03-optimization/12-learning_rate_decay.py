c7806b647501d8ff4088e56bfa1904c51901aa35
miguelfajardoc <775@holbertonschool.com>
2020-08-24 05:16:04 +0000

    [add] decay tensor

--- supervised_learning/0x03-optimization/12-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -18,4 +18,4 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     decay_optimizer = tf.train.inverse_time_decay(alpha, global_step,
                                                   decay_steps, decay_rate,
                                                   epsilon=epsilon)
-    return decay_optimizer.minimize(loss)
+    return decay_optimizer
