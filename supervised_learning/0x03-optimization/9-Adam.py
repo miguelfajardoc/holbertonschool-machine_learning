6908430ca15f4278e250940cfa323808edf54bac
miguelfajardoc <775@holbertonschool.com>
2020-08-24 04:55:18 +0000

    [add] adam algorithm

--- supervised_learning/0x03-optimization/9-Adam.py
+++ supervised_learning/0x03-optimization/9-Adam.py
@@ -21,10 +21,10 @@ def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     moment, respectively
     """
     V = beta1 * v + (1 - beta1) * grad
-    correctionV = 1 - beta1 ** (t + 1)
+    correctionV = 1 - beta1 ** (t)
     Vcorr = V / correctionV
     S = beta2 * s + (1 - beta2) * grad ** 2
-    correctionS = 1 - beta2 ** (t + 1)
+    correctionS = 1 - beta2 ** (t)
     Scorr = S / correctionS
     updated_var = var - alpha * ((Vcorr) / ((Scorr) ** (1 / 2) + epsilon))
     return updated_var, V, S
