308b221cce435b8d3bd33335aeee27a9cfca1cd2
miguelfajardoc <775@holbertonschool.com>
2020-08-25 05:00:02 +0000

    [add] identation

--- supervised_learning/0x04-error_analysis/4-f1_score.py
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -7,17 +7,17 @@ precision = __import__('2-precision').precision
 
 
 def f1_score(confusion):
-        """
-        calculates the F1 score of a confusion matrix
-        - confusion: numpy.ndarray of shape (classes, classes) where row
-        indices represent the correct labels and column indices represent
-        the predicted labels.
-        - classes is the number of classes
-        Returns: a numpy.ndarray of shape (classes,) containing the F1 score
-        of each class
-        """
-        sens = sensitivity(confusion)
-        prcs = precision(confusion)
-        f1_score = 2 * sens * prcs / (sens + prcs)
+    """
+    calculates the F1 score of a confusion matrix
+    - confusion: numpy.ndarray of shape (classes, classes) where row
+    indices represent the correct labels and column indices represent
+    the predicted labels.
+    - classes is the number of classes
+    Returns: a numpy.ndarray of shape (classes,) containing the F1 score
+    of each class
+    """
+    sens = sensitivity(confusion)
+    prcs = precision(confusion)
+    f1_score = 2 * sens * prcs / (sens + prcs)
 
-        return f1_score
+    return f1_score
