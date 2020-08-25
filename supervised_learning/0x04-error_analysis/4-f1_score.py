bf7197e9f71357e863e5574a959a5ce2ab40746b
miguelfajardoc <775@holbertonschool.com>
2020-08-25 04:49:46 +0000

    [add] f1

--- supervised_learning/0x04-error_analysis/4-f1_score.py
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Module to calculate a F1 from a confusion matrix"""
 
+import numpy as np
 sensitivity = __import__('1-sensitivity').sensitivity
 precision = __import__('2-precision').precision
 
