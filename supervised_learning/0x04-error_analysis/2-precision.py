6cf782186492726ecb32ba11ef26be95114a1b67
miguelfajardoc <775@holbertonschool.com>
2020-08-25 04:15:19 +0000

    [add] style

--- supervised_learning/0x04-error_analysis/2-precision.py
+++ supervised_learning/0x04-error_analysis/2-precision.py
@@ -5,7 +5,8 @@ import numpy as np
 
 def precision(confusion):
     """
-    function that calculates the precision for each class in a confusion matrix:
+    function that calculates the precision for each class in a confusion
+    matrix:
     confusion is a confusion numpy.ndarray of shape (classes, classes) where
     row indices represent the correct labels and column indices represent the
     predicted labels
