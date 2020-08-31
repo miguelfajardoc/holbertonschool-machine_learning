c6f5abe22ec691ff9b86920d87c158104ef023d9
miguelfajardoc <miguelanfac@gmail.com>
2020-08-30 19:16:37 -0500

    [add] regularization cost tensor flow

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ modulo to regularization con tensor flow """
 
+import tensorflow as tf
+
 
 def l2_reg_cost(cost):
     """
@@ -11,4 +13,4 @@ def l2_reg_cost(cost):
     Returns: a tensor containing the cost of the network accounting
     for L2 regularization
     """
-    return cost.get_regularization_losses()
+    return cost + tf.losses.get_regularization_losses()
