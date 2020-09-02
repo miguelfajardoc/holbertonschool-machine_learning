87cf5952343ccf730447a325e7b50672cc3c1dfc
miguelfajardoc <775@holbertonschool.com>
2020-09-01 23:18:56 -0500

    [add] optimize

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -17,6 +17,6 @@ def optimize_model(network, alpha, beta1, beta2):
     """
     adam = K.optimizers.Adam(lr=alpha, beta_1=beta1, beta_2=beta2)
     network.compile(optimizer=adam,
-                  loss='categorical_crossentropy',
-                  metrics=["accuracy"])
+                    loss='categorical_crossentropy',
+                    metrics=["accuracy"])
     return None
