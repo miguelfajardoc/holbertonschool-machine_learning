31e54fb76b928019feaac7b65282c12cb4c82e07
miguelfajardoc <775@holbertonschool.com>
2020-09-01 21:53:53 -0500

    [add] sequential

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -7,7 +7,8 @@ import tensorflow.keras as K
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """function that builds a neural network with the Keras library:
     - nx is the number of input features to the network
-    - layers is a list containing the number of nodes in each layer of the network
+    - layers is a list containing the number of nodes in each layer of the
+    network
     - activations is a list containing the activation functions used for each
     layer of the network
     - lambtha is the L2 regularization parameter
